de945b2aa7483ae4e4f368bd75597eca7b4bb3ba
Molly <molly.leitner@downstate.edu>
2023-09-14 16:43:44 -0400

    added mechanisms, arm64 and trace files

---
+++ nbatch_traces/amp_netParams.py
@@ -0,0 +1,34 @@
+from netpyne import specs, sim
+
+import json
+
+try:
+    from __main__ import cfg
+except:
+    from cfg import cfg
+
+"""
+netParams
+"""
+
+netParams = specs.NetParams()
+
+
+fptr = open("na1612r1.json", "r")
+hhcell = json.load(fptr)
+fptr.close()
+
+hhcell['conds'] = {'cellType': 'hh'}
+
+netParams.cellParams['hh'] = hhcell
+
+
+"""
+stimulation parameters
+"""
+
+# add stim source
+netParams.stimSourceParams['ic'] = {'type': 'IClamp', 'delay': 300, 'dur': 1000, 'amp': cfg.amp}
+# connect stim source to target
+netParams.stimTargetParams['ic->hh'] = {'source': 'ic', 'conds': {'pop': 'hh'}, 'sec': 'soma_0', 'loc': 0.5}
+netParams.popParams['hh'] = {'cellType': 'hh', 'numCells': 1}
