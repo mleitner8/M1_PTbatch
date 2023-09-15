7d61cb9850a9bbc12dbc02e732a2465656d469fd
Molly <molly.leitner@downstate.edu>
2023-09-15 08:03:49 -0400

    all new batch files

---
+++ arm64/analysis.py
@@ -0,0 +1,12 @@
+"""
+analysis.py
+
+Functions to read and plot figures from the batch simulation results.
+Can call readPlotNa() or readPlotNMDA() from __main__() or import analysis and call interactively.
+"""
+
+import utils
+import json
+import matplotlib.pyplot as plt
+
+def plot_FI():
